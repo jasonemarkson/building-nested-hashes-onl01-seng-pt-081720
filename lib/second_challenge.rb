03190ab224893c70239d3004ddc87037d3181590
Jason Markson <jasonmarkson76@gmail.com>
2020-08-29 16:05:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {:name = "Lord Montague", :age = "53"}, 
-      :matriarch => {:name = "Lady Montague", :age = "54"},  
-      :hero => {:name = "Romeo", :age = "15", :status = "alive"}, 
+      :patriarch => {}, 
+      :matriarch => {},  
+      :hero => {}, 
       :hero_friends => [] 
       }, 
     :capulet => {
